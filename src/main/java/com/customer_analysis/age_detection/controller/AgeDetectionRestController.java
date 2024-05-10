b2a32d1b5a7e4114c9cc1813bb6b3ed3175dcc83
Metin Gökmen <metingokmen031@gmail.com>
2024-05-11 01:17:29 +0300

    Spring update and remove unnecessary imports.

--- src/main/java/com/customer_analysis/age_detection/controller/AgeDetectionRestController.java
+++ src/main/java/com/customer_analysis/age_detection/controller/AgeDetectionRestController.java
@@ -1,7 +1,6 @@
 package com.customer_analysis.age_detection.controller;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
